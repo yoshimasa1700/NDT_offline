48b68479be6791067d11e21ed0dc16fb3d441f71
Masahiko Toyoshi <Masahiko.Toyoshi@sony.com>
2022-04-10 11:07:15 +0900

    Add simple test

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,15 @@
 #include "gtest/gtest.h"
+#include <ndt_scan_matcher.hpp>
 
-TEST(TestCaseName, TestName) {
-    EXPECT_EQ(1,1);
+
+TEST(TestAxisSort, Simple1) {
+
+    point_with_id a, b;
+
+    unsigned int axis = 0;
+
+    a.pos[axis] = 2.0;
+    b.pos[axis] = 1.0;
+
+    EXPECT_EQ(AxisSort(axis, a, b), true);
 }
